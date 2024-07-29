92247e11f6543a01c713f48bb43f3a87a5a40b09
tinypixel <him23code@gmail.com>
2024-07-29 08:39:42 +0530

    Initial Commit

---
+++ Monthly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Monthly
+//
+//  Created by Himanshu Singh on 29/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Himanshu Singh on 29/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
